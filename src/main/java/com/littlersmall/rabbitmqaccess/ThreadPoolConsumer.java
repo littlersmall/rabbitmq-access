8e3f256f3db1b66d981eb1d4ccef862e6ae3dde3
xingyi <xingyi@kuaishou.com>
2020-08-07 14:41:18 +0800

    调整依赖

--- src/main/java/com/littlersmall/rabbitmqaccess/ThreadPoolConsumer.java
+++ src/main/java/com/littlersmall/rabbitmqaccess/ThreadPoolConsumer.java
@@ -8,8 +8,6 @@ import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.omg.SendingContext.RunTime;
-
 /**
  * Created by littlersmall on 16/5/23.
  */
